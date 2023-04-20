3573c9f034588c3d32928ae022141878fc3fb3ac
mickdon94 <131375083+mickdon94@users.noreply.github.com>
2023-04-20 15:28:58 +0100

    Update streamlit_app.py
    Found typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,11 @@ streamlit.header('BREAKFAST MENU')
 streamlit.text('🥣 OMEGA 3 & BLUEBERRY OATMEAL')
 streamlit.text('🥗 KALE, SPINACH & ROCKET SMOOTHIE')
 streamlit.text('🐔HARD-BOILED FREE-RANGE EGG')
-streamlit.test('🥑🍞 AVOCADO TOAST')
-               
+streamlit.text('🥑🍞 AVOCADO TOAST')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
